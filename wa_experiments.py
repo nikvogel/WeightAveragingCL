5d8e80830f36087987ca115ae9e7659884820ceb
Nik Vogel <74900005+nikvogel@users.noreply.github.com>
2022-12-02 22:50:06 +0100

    Change experimental design for wa_experiments to lower computational effort

--- wa_experiments.py
+++ wa_experiments.py
@@ -8,18 +8,36 @@ no_experiences = [5, 10, 20]
 weighting_methods = ['average', 'inverted_t', 'inverted_t_adapted']
 
 for no_experience in no_experiences:
-    for epoch in epochs:
-        for learning_rate in learning_rates:
-            for hidden_size in hidden_sizes:
-                for hidden_layer in hidden_layers:
-                    for weighting_method in weighting_methods:
-                        overwrite_dict = {'epochs': epoch,
-                                          'learning_rate': learning_rate,
-                                          'hidden_size': hidden_size,
-                                          'hidden_layers': hidden_layer,
-                                          'no_experiences': 10,
-                                          'weighting_method': weighting_method,
-                                          'log_path': f'./logs/wa_exp_pmnist/ex{no_experience}_ep{epoch}_lr'\
-                                                      f'{learning_rate}_hs{hidden_size}_hl{hidden_layer}'\
-                                                      f'_{weighting_method}'}
-                        wa_pmnist(overwrite_dict)
+    overwrite_dict = {'no_experiences': no_experience,
+                      'log_path': f'./logs/wa_exp_pmnist/no_experiences/{no_experience}/'}
+    wa_pmnist(overwrite_dict)
+
+for epoch in epochs:
+    overwrite_dict = {'epochs': epoch,
+                      'log_path': f'./logs/wa_exp_pmnist/epochs/{epoch}/'}
+    wa_pmnist(overwrite_dict)
+
+for learning_rate in learning_rates:
+    overwrite_dict = {'learning_rate': learning_rate,
+                      'log_path': f'./logs/wa_exp_pmnist/learning_rate/{learning_rate}/'}
+    wa_pmnist(overwrite_dict)
+
+for hidden_size in hidden_sizes:
+    overwrite_dict = {'hidden_size': hidden_size,
+                      'log_path': f'./logs/wa_exp_pmnist/hidden_size/{hidden_size}/'}
+    wa_pmnist(overwrite_dict)
+
+for hidden_layer in hidden_layers:
+    overwrite_dict = {'hidden_layers': hidden_layer,
+                      'log_path': f'./logs/wa_exp_pmnist/hidden_layers/{hidden_layer}/'}
+    wa_pmnist(overwrite_dict)
+
+for no_experience in no_experiences:
+    overwrite_dict = {'no_experiences': no_experience,
+                      'log_path': f'./logs/wa_exp_pmnist/experiences/{no_experience}/'}
+    wa_pmnist(overwrite_dict)
+
+for weighting_method in weighting_methods:
+    overwrite_dict = {'weighting_method': weighting_method,
+                      'log_path': f'./logs/wa_exp_pmnist/weighting_method/{weighting_method}/'}
+    wa_pmnist(overwrite_dict)

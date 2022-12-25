1134e92a4f40b1d4eea54b684a66e650eb79bdbb
Niklas Vogel <74900005+nikvogel@users.noreply.github.com>
2022-12-25 23:47:36 +0100

    Update wrapper for Permuted MNIST

--- experiments/permuted_mnist_simple/__init__.py
+++ experiments/permuted_mnist_simple/__init__.py
@@ -2,3 +2,4 @@ from .ewc import ewc_pmnist_simple
 from .naive import naive_pmnist_simple
 from .wa import wa_pmnist_simple
 from .cumulative import cumulative_pmnist_simple
+from .lwf import lwf_pmnist_simple

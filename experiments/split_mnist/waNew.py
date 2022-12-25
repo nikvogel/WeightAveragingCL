b3d0260ee1e76d50a986e91e1c10e595cabcfbc3
Niklas Vogel <74900005+nikvogel@users.noreply.github.com>
2022-12-25 23:45:28 +0100

    Improve and add new Split MNIST experiments

--- experiments/split_mnist/waNew.py
+++ experiments/split_mnist/waNew.py
@@ -1,39 +1,41 @@
 import avalanche as avl
 import torch
 from torch.nn import CrossEntropyLoss
-from torch.optim import Adam
+from torch.optim import SGD
 from avalanche.evaluation import metrics as metrics
-from models import MultiHeadMLP, MLP
+from models import MLP, WeightAveragingPlugin
 from experiments.utils import set_seed, create_default_args
 
+def wa_smnist_new(override_args=None):
+    """
+    "Learning without Forgetting" by Li et. al. (2016).
+    http://arxiv.org/abs/1606.09282
+    Since experimental setup of the paper is quite outdated and not
+    easily reproducible, this experiment is based on
+    "Three scenarios for continual learning" by van de Ven et. al. (2018).
+    https://arxiv.org/pdf/1904.07734.pdf
 
-def naive_smnist(override_args=None):
+    The hyper-parameter alpha controlling the regularization is increased over time, resulting
+    in a regularization of  (1- 1/n_exp_so_far) * L_distillation
     """
-    "Continual Learning Through Synaptic Intelligence" by Zenke et. al. (2017).
-    http://proceedings.mlr.press/v70/zenke17a.html
-    """
-    args = create_default_args({'cuda': 1,
+    args = create_default_args({'cuda': 0,
                                 'epochs': 10,
-                                'learning_rate': 0.001,
+                                'layers': 1, 
+                                'hidden_size': 500,
+                                'learning_rate': 0.001, 
                                 'train_mb_size': 256,
                                 'eval_mb_size': 128,
-                                'seed': 0,
-                                'hidden_size': 1000,
-                                'hidden_layers': 2,
-                                'no_experiences': 10,
-                                'task_incremental': True,
-                                'log_path': './logs/s_mnist/naive/'}, override_args)
+                                'no_experiences': 5,
+                                'log_path': './logs/s_mnist/waNew/',
+                                'seed': 0}, override_args)
     set_seed(args.seed)
     device = torch.device(f"cuda:{args.cuda}"
                           if torch.cuda.is_available() and
                           args.cuda >= 0 else "cpu")
 
-    benchmark = avl.benchmarks.classic.SplitMNIST(args.no_experiences, return_task_id=args.task_incremental,
-                                          fixed_class_order=list(range(10)))
-
-    model = MultiHeadMLP(hidden_size=args.hidden_size, hidden_layers=args.hidden_layers) if args.task_incremental \
-        else MLP(hidden_size=args.hidden_size, hidden_layers=args.hidden_layers)
-
+    benchmark = avl.benchmarks.SplitMNIST(args.no_experiences, return_task_id=False)
+    model = MLP(hidden_size=args.hidden_size, hidden_layers=args.layers,
+                initial_out_features=0, relu_act=False)
     criterion = CrossEntropyLoss()
 
     interactive_logger = avl.logging.InteractiveLogger()
@@ -50,17 +52,18 @@ def naive_smnist(override_args=None):
         loggers=[interactive_logger, csv_logger, text_logger, tensorboard_logger])
 
     cl_strategy = avl.training.Naive(
-        model, Adam(model.parameters(), lr=args.learning_rate), criterion,
-        train_mb_size=args.train_mb_size, train_epochs=args.epochs, eval_mb_size=args.eval_mb_size,
-        device=device, evaluator=evaluation_plugin)
+        model, SGD(model.parameters(), lr=args.learning_rate), criterion,
+        train_mb_size=args.train_mb_size, train_epochs=args.epochs,
+        device=device, evaluator=evaluation_plugin, plugins=[WeightAveragingPlugin()])
 
+    res = None
     for experience in benchmark.train_stream:
         cl_strategy.train(experience)
-        result = cl_strategy.eval(benchmark.test_stream)
+        res = cl_strategy.eval(benchmark.test_stream)
 
-    return result
+    return res
 
 
 if __name__ == '__main__':
-    res = naive_smnist()
+    res = wa_smnist_new()
     print(res)

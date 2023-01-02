54404e598a046ebfcc6823b2c459b976c308ca12
Niklas Vogel <74900005+nikvogel@users.noreply.github.com>
2023-01-02 11:05:43 +0100

    Fit init file for Split Tiny Imagenet

--- experiments/split_tiny_imagenet/__init__.py
+++ experiments/split_tiny_imagenet/__init__.py
@@ -1,3 +1,9 @@
 from .lwf import lwf_stinyimagenet
 from .naive import naive_stinyimagenet
 from .wa import wa_stinyimagenet
+'''
+TODO:
+    - Unify hyperparameters for experiments w/ aplit tiny imagenet
+    - Apply code style of other experiments/ other datasets
+    - Update hyperparameters for weight averaging (reduce numbers of epochs)
+'''
